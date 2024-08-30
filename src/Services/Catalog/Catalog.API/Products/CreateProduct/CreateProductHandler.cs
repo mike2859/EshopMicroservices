36be333c07a0cbe8ffa49ebb816085b0c6f72241
test <supermike18@msn.com>
2024-08-30 23:17:06 +0200

    Add Document session for postgres database Add global usings Add Marten nugets with connectionString postgres

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler 
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -23,10 +20,11 @@ internal class CreateProductCommandHandler
         };
 
         //Save the database
-        
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         //return CreateProductResult result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
 
     }
 }
