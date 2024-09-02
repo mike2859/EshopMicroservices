8f72f5ac3b4db7477be681ec043b9d33e25b9c42
test <supermike18@msn.com>
2024-09-02 23:05:21 +0200

    Add ValidationBehaviors

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<TRequest, Tresponse>
+    (IEnumerable<IValidator<TRequest>>  validators)
+    : IPipelineBehavior<TRequest, Tresponse>
+    where TRequest : ICommand<TRequest>
+{
+    public async Task<Tresponse> Handle(TRequest request, RequestHandlerDelegate<Tresponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = 
+            await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults
+            .Where(r => r.Errors.Any())
+            .SelectMany(r => r.Errors)
+            .ToList();
+
+        if (failures.Any())
+            throw new ValidationException(failures);
+
+        return await next();
+    }
+}
