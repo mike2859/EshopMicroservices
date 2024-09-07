a7e3f8b7eb2f29b7c2f66668e5ef7e17be358903
test <supermike18@msn.com>
2024-09-07 22:25:29 +0200

    Add GRPC microservice Implement Grpc microservice

---
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace Discount.Grpc.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = default!;
+    public string ProductDescription { get; set; }=default!;
+    public int Amount { get; set; }
+}
