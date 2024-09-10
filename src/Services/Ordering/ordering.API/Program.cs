67fa203323eb1898558523259ec97180a2b58e89
test <supermike18@msn.com>
2024-09-11 00:24:15 +0200

    Add Differents dependencies injection

--- src/Services/Ordering/ordering.API/Program.cs
+++ src/Services/Ordering/ordering.API/Program.cs
@@ -1,6 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container (Injection)
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 var app = builder.Build();
 
