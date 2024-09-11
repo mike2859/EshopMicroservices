a61fcd38b3a269531196fe61e44283541e998877
test <supermike18@msn.com>
2024-09-11 23:33:41 +0200

    Add Entities and Aggregate ValueObjects

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string? CardName { get; private set; } = default!;
+    public string CardNumber { get; private set; } = default!;
+    public string Expiratiàon { get; private set; } = default!;
+    public string CVV { get; private set; } = default!;
+    public int PaymentMethod { get; private set; } = default!;
+
+}
