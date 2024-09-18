75e06670dc84d0484c4e3df0f15a90572808ecdb
test <supermike18@msn.com>
2024-09-18 23:52:08 +0200

    Add Dtos Objects

---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Dtos;
+
+public record OrderDto(
+    Guid Id,
+    Guid CustomerId,
+    AddressDto ShippingAddress,
+    AddressDto BillinAddress,
+    PaymentDto Payment,
+    OrderStatus Status,
+    List<OrderItemDto> OrderItems);
---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record OrderItemDto(Guid OrderId, Guid ProductId, int Quentity, decimal Price);
