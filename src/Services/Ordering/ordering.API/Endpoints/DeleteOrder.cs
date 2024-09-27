f91053de26c8352c3dd02b7b81742a9fdc27cddf
test <supermike18@msn.com>
2024-09-27 22:49:05 +0200

    Delete order

--- src/Services/Ordering/ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/ordering.API/Endpoints/DeleteOrder.cs
@@ -14,7 +14,7 @@ public class DeleteOrder : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapDelete("/order/{id}", async (Guid Id, ISender sender) =>
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
         {
             var result = await sender.Send(new DeleteOrderCommand(Id));
 
